22d2f0ef6fde6739ad5ac663306574627e91cd3a
ribelo <r.krzywaznia@gmail.com>
2020-04-20 09:46:46 +0200

    init

---
+++ src/main/ribelo/fireposh/events.cljs
@@ -0,0 +1,73 @@
+(ns ribelo.fireposh.events
+  (:require
+   [re-frame.core :as rf]
+   [re-posh.core :as rp]
+   [datascript.core :as d]
+   [ribelo.firenze.firebase :as fb]
+   [ribelo.fireposh.fx :as fx]
+   [ribelo.firenze.realtime-database :as rdb]
+   [applied-science.js-interop :as j]
+   [cljs-bean.core :refer [bean ->js ->clj]]))
+
+(rf/reg-event-fx
+ ::init-firebase
+ (fn [_ [_ app-info]]
+   {::fx/init-firebase app-info}))
+
+(rf/reg-event-fx
+ ::create-connection.firebase-schema
+ (fn [_ _]
+   {::fx/create-connection.firebase-schema nil}))
+
+(rf/reg-event-fx
+ ::create-connection.local-schema
+ (fn [_ [_ schema]]
+   {::fx/create-connection.local-schema schema}))
+
+(rf/reg-event-fx
+ ::set-schema
+ (fn [_ [_ schema]]
+   {::fx/set-schema schema}))
+
+(rf/reg-event-fx
+ ::link-max-eid
+ (fn [_ _]
+   {::fx/link-max-eid nil}))
+
+(rf/reg-event-fx
+ ::link-db
+ (fn [_ [_]]
+   {::fx/link-db nil}))
+
+(rf/reg-event-fx
+ ::link-path
+ (fn [_ [_ path]]
+   {::fx/link-paths [path]}))
+
+(rf/reg-event-fx
+ ::link-paths
+ (fn [_ [_ paths]]
+   {::fx/link-paths paths}))
+
+(rf/reg-event-fx
+ ::unlink-path
+ (fn [_ [_ path]]
+   {::fx/unlink-paths [path]}))
+
+(rf/reg-event-fx
+ ::unlink-paths
+ (fn [_ [_ paths]]
+   {::fx/unlink-paths paths}))
+
+(rf/reg-event-fx
+ :transact!
+ (fn [_ [_ tx-data]]
+   {:transact tx-data}))
+
+(rf/reg-event-fx
+ ::init
+ (fn [_ [_ app-info]]
+   {:dispatch-n [[::init-firebase app-info]
+                 [::create-connection.firebase-schema]
+                 [::link-max-eid]
+                 [::link-db]]}))
---
+++ src/main/ribelo/fireposh/fx.cljs
@@ -0,0 +1,110 @@
+(ns ribelo.fireposh.fx
+  (:require
+   [clojure.walk :refer [postwalk]]
+   [re-frame.core :as rf]
+   [re-posh.core :as rp]
+   [datascript.core :as d]
+   [ribelo.firenze.firebase :as fb]
+   [ribelo.firenze.realtime-database :as rdb]
+   [applied-science.js-interop :as j]
+   [cljs-bean.core :refer [bean ->js ->clj]]))
+
+(rf/reg-fx
+ ::init-firebase
+ (fn [app-info]
+   (fb/initialize-app app-info)))
+
+(rf/reg-fx
+ ::create-connection.firebase-schema
+ (fn [_]
+   (-> (rdb/ref [:_meta :schema])
+       (j/call :once "value"
+               (fn [snap]
+                 (let [schema
+                       (postwalk (fn [x]
+                                   (if (map? x) (into {} (map (fn [[k v]]
+                                                                [(keyword (demunge k))
+                                                                 (if (string? v) (keyword (demunge v)) v)]) x)) x))
+                                 (bean (j/call snap :val) :keywordize-keys false :recursive true))]
+                   (rf/dispatch [::create-connection.local-schema schema])))))))
+
+(rf/reg-fx
+ ::create-connection.local-schema
+ (fn [schema]
+   (let [schema' (or schema ( [:_meta :schema]))
+         conn    (d/create-conn schema)]
+     (rp/connect! conn))))
+
+(rf/reg-fx
+ ::set-schema
+ (fn [schema]
+   (rdb/set [:_meta :schema]
+            (postwalk (fn [x]
+                        (if (map? x) (into {} (map (fn [[k v]]
+                                                     [(munge (->js k))
+                                                      (if (keyword? v) (munge (->js v)) v)]) x)) x))
+                      schema))))
+
+(rf/reg-fx
+ ::link-max-eid
+ (fn [_]
+   (let [conn @re-posh.db/store]
+     (rdb/on :value [:_meta :max-eid] #(swap! conn assoc :max-eid %))
+     (d/listen! conn ::link-max-eid
+                (fn [{:keys [tx-data]}]
+                  (let [max-eid (->> tx-data last first)]
+                    (rdb/set [:_meta :max-eid] max-eid)))))))
+
+(rf/reg-fx
+ ::link-db
+ (fn [_]
+   (d/listen! @re-posh.db/store ::link-db
+              (fn [{:keys [db-before db-after tx-data tx-meta]}]
+                (when-not (= ::sync tx-meta)
+                  (doseq [[e a v tx added?] tx-data]
+                    (when-let [path (:db/path (d/entity (if added? db-after db-before) e))]
+                      (if added?
+                        (rdb/set (conj path e a) v)
+                        (rdb/remove (conj path e a))))))))))
+
+(defn- on-child-added [conn snap]
+  (let [e (js/parseInt (j/get snap :key))
+        m (persistent!
+           (reduce-kv
+            (fn [acc k v]
+              (assoc! acc (keyword (demunge k)) (->clj v)))
+            (transient {})
+            (bean (j/call snap :val) :keywordize-keys false)))]
+    (d/transact! conn [(assoc m :db/id e)] ::sync)))
+
+(defn- on-child-changed [conn snap]
+  (let [e (j/get snap :key)
+        m (persistent!
+           (reduce-kv
+            (fn [acc k v]
+              (assoc! acc (keyword (demunge k)) (->clj v)))
+            (transient {})
+            (bean (j/call snap :val) :keywordize-keys false)))]
+    (d/transact! conn [(assoc m :db/id e)] ::sync)))
+
+(defn- on-child-removed [conn snap]
+  (let [e (js/parseInt (j/get snap :key))]
+    (d/transact! conn [[:db.fn/retractEntity e]] ::sync)))
+
+(rf/reg-fx
+ ::link-paths
+ (fn [paths]
+   (doseq [path paths]
+     (rdb/off path)
+     (-> (rdb/ref path)
+         (j/call :on "child_added"   (partial on-child-added   @re-posh.db/store)))
+     (-> (rdb/ref path)
+         (j/call :on "child_changed" (partial on-child-changed @re-posh.db/store)))
+     (-> (rdb/ref path)
+         (j/call :on "child_removed" (partial on-child-removed @re-posh.db/store))))))
+
+(rf/reg-fx
+ ::unlink-paths
+ (fn [paths]
+   (doseq [path paths]
+     (rdb/off path))))
