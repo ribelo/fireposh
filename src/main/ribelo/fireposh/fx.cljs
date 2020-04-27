33d199fb05f1dc2433f410c83fb7dc3ad75d478b
ribelo <r.krzywaznia@gmail.com>
2020-04-27 08:47:02 +0200

    Changes to be committed: 	modified:   src/main/ribelo/fireposh/fx.cljs

--- src/main/ribelo/fireposh/fx.cljs
+++ src/main/ribelo/fireposh/fx.cljs
@@ -1,6 +1,7 @@
 (ns ribelo.fireposh.fx
   (:require
    [clojure.walk :refer [postwalk]]
+   [taoensso.timbre :as timbre]
    [re-frame.core :as rf]
    [re-posh.core :as rp]
    [datascript.core :as d]
@@ -9,6 +10,8 @@
    [ribelo.firenze.utils :as fu]
    [applied-science.js-interop :as j]
    [cljs-bean.core :refer [bean ->js ->clj]]))
+
+(def ^:private ids-map_ (atom {}))
 
 (rf/reg-fx
  ::init-firebase
@@ -41,58 +44,75 @@
  (fn [schema]
    (rdb/set [:_meta :schema]
             (postwalk (fn [x]
-                        (if (map? x) (into {} (map (fn [[k v]]
-                                                     [(fu/munge k)
-                                                      (if (keyword? v) (fu/munge v) v)]) x)) x))
+                        (if (map? x)
+                          (into {} (map
+                                    (fn [[k v]]
+                                      [(fu/munge k)
+                                       (if (keyword? v) (fu/munge v) v)]) x))
+                          x))
                       schema))))
-
-(rf/reg-fx
- ::link-max-eid
- (fn [_]
-   (let [conn @re-posh.db/store]
-     (rdb/on :value [:_meta :max-eid] #(swap! conn assoc :max-eid %))
-     (d/listen! conn ::link-max-eid
-                (fn [{:keys [tx-data]}]
-                  (let [max-eid (:max-eid @conn)
-                        new-max-eid (->> tx-data last first)]
-                    (when (> new-max-eid max-eid)
-                      (rdb/set [:_meta :max-eid] max-eid))))))))
 
 (rf/reg-fx
  ::link-db
  (fn [_]
    (d/listen! @re-posh.db/store ::link-db
-              (fn [{:keys [db-before db-after tx-data tx-meta]}]
+              (fn [{:keys [db-after tx-data tx-meta tempids]}]
+                (doseq [[_ eid] (dissoc tempids :db/current-tx)]
+                  (when-not (get @ids-map_ eid)
+                    (swap! ids-map_ assoc eid (str (d/squuid)))))
                 (when-not (= ::sync tx-meta)
-                  (doseq [[e a v tx added?] tx-data]
-                    (when-let [path (:db/path (d/entity (if added? db-after db-before) e))]
-                      (if added?
-                        (rdb/set (conj path e a) v)
-                        (rdb/remove (conj path e a))))))))))
+                  ;; TODO add remove from ids-map_ when retract entity
+                  (doseq [eid (into [] (comp (map first) (distinct)) tx-data)]
+                    (let [{:keys [db/path]
+                           :as   m} (d/pull db-after '[*] eid)]
+                      (when path
+                        (let [refs (:db.type/ref (:rschema db-after))
+                              fid  (get @ids-map_ eid)
+                              m'   (persistent!
+                                    (reduce-kv
+                                     (fn [acc k v]
+                                       (if-not (refs k)
+                                         (assoc! acc (fu/munge k) (->js v))
+                                         (if-let [fid (get @ids-map_ (:db/id v))]
+                                           (assoc! acc (fu/munge k) fid)
+                                           (do
+                                             (timbre/error :link-db (str "no fid for eid: " (:db/id v)))
+                                             acc))))
+                                     (transient {})
+                                     (dissoc m :db/id)))]
+                          (rdb/set (conj path fid) (->js m')))))))))))
 
 (defn- on-child-added [conn snap]
-  (let [e (js/parseInt (j/get snap :key))
-        m (persistent!
-           (reduce-kv
-            (fn [acc k v]
-              (assoc! acc (fu/demunge k) (->clj v)))
-            (transient {})
-            (bean (j/call snap :val) :keywordize-keys false)))]
-    (d/transact! conn [(assoc m :db/id e)] ::sync)))
-
-(defn- on-child-changed [conn snap]
-  (let [e (j/get snap :key)
-        m (persistent!
-           (reduce-kv
-            (fn [acc k v]
-              (assoc! acc (fu/demunge k) (->clj v)))
-            (transient {})
-            (bean (j/call snap :val) :keywordize-keys false)))]
-    (d/transact! conn [(assoc m :db/id e)] ::sync)))
+  (let [fid               (demunge (j/get snap :key))
+        eid               (get (clojure.set/map-invert @ids-map_) fid fid)
+        refs              (:db.type/ref (:rschema @conn))
+        m                 (persistent!
+                           (reduce-kv
+                            (fn [acc k v]
+                              (let [k* (fu/demunge k)
+                                    v* (->clj v)]
+                                (if-not (refs k*)
+                                  (assoc! acc k* v*)
+                                  (if-let [ref-eid (get (clojure.set/map-invert @ids-map_) (demunge v*))]
+                                    (assoc! acc k* ref-eid)
+                                    (assoc! acc k* v*)))))
+                            (transient {:db/id eid})
+                            (bean (j/call snap :val) :keywordize-keys false)))
+        {:keys [db-after tx-data tx-meta
+                tempids]} (d/with @conn [m])]
+    (doseq [[fid eid] (dissoc tempids :db/current-tx)]
+      (when-not (get @ids-map_ eid)
+        (swap! ids-map_ assoc eid fid)))
+    ;; TODO listener callback
+    (reset! conn db-after)))
 
 (defn- on-child-removed [conn snap]
-  (let [e (js/parseInt (j/get snap :key))]
-    (d/transact! conn [[:db.fn/retractEntity e]] ::sync)))
+  (let [fid (demunge (j/get snap :key))]
+    (if-let [eid (get (clojure.set/map-invert @ids-map_) fid)]
+      (do
+        (swap! ids-map_ dissoc eid)
+        (d/transact! conn [[:db.fn/retractEntity eid]])) ; with sync, refs must be updated
+      (timbre/error :on-child-removed (str "no eid for fid: " fid)))))
 
 (rf/reg-fx
  ::link-paths
@@ -102,7 +122,7 @@
      (-> (rdb/ref path)
          (j/call :on "child_added"   (partial on-child-added   @re-posh.db/store)))
      (-> (rdb/ref path)
-         (j/call :on "child_changed" (partial on-child-changed @re-posh.db/store)))
+         (j/call :on "child_changed" (partial on-child-added @re-posh.db/store)))
      (-> (rdb/ref path)
          (j/call :on "child_removed" (partial on-child-removed @re-posh.db/store))))))
 
